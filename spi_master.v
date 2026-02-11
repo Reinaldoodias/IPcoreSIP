c0db06d85fc875b75b0a98a30d10d570996f3fed
Andre Maciel <109044816+andrestruct@users.noreply.github.com>
2026-02-11 00:14:58 -0300

    feat: adição dos registradores internos de controle e contadores

--- spi_master.v
+++ spi_master.v
@@ -24,4 +24,33 @@ module spi_master
     // Interface SPI (todos os sinais relacionados operam no domínio do clock SPI)
     wire w_CPOL;    // como o clock começa - estado ocioso
     wire w_CPHA;   // se você lê no primeiro ou segundo movimento
+
+
+    // ================================
+    // Registradores e contadores
+    // ================================
+    reg [$clog2(CICLOS_POR_MEIO_BIT*2)-1:0] contador_clk;
+    reg [4:0] contador_bordas; // Conta o número de bordas do clock SPI (subida + descida)
+
+    reg clk_spi_interno; // Clock SPI interno antes de ser enviado para o pino spi_clk
+    reg borda_subida; // Indica quando ocorreu uma borda de subida do clock SPI
+    reg borda_descida; // Indica quando ocorreu uma borda de descida do clock SPI
+
+
+    // Registrador que armazena o byte a ser transmitido via MOSI
+    // Funciona como um shift register durante a transmissão
+    reg [7:0] registrador_tx;
+
+    // Guarda o estado do sinal tx_valido internamente
+    // Evita perder o pedido de transmissão enquanto o SPI está ocupado
+    reg       tx_valido_reg;
+
+    // Contador de bits transmitidos (0 a 7)
+    // Controla qual bit do registrador_tx está sendo enviado
+    reg [2:0] contador_bit_tx;
+
+    // Contador de bits recebidos (0 a 7)
+    // Controla a montagem do byte recebido via MISO
+    reg [2:0] contador_bit_rx;
+
 );
